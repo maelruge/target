33ca2ad2ddcf2a3d915712845b4e8728c644c19d
Ruzgar <ruzgaryaranir@gmail.com>
2026-09-11 22:05:40 +0300

    2.phase

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cv2
 import mediapipe as mp
 
+
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5)
+hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7)
+
+img = cv2.imread("ilkkan.jpeg")
 
 camera = cv2.VideoCapture(0)
 
@@ -10,6 +13,8 @@ while True:
     ret, frame = camera.read()
     if not ret:
         break
+
+    HandOpen = False
 
     frame = cv2.flip(frame, 1)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -28,7 +33,18 @@ while True:
                 open_leg += 1
 
         if open_leg == 4:
-            print("picture is ok")
+            HandOpen = True
+        if HandOpen:
+
+            if img is not None:
+                cv2.imshow("img", img)
+
+        else:
+            try:
+             cv2.destroyWindow("img")
+            except cv2.error:
+             pass
+
 
     cv2.imshow("Camera", frame)
 
